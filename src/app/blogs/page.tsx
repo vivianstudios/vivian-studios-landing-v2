3e78d3fdfb7580ff63e443cb5350cb3b3ee4f82e
noelalam9999 <noel.alam9999@gmail.com>
2024-10-28 22:54:14 +0600

    feat:blogs

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import BlogCard, { BlogCardProps } from "../components/BlogCard";
 import Rigging from "@/app/assets/animation/Rigging.jpg";
 import { getBlogs } from "@/services/blogs";
-type BlogsProps = {
-  posts: BlogCardProps[];
-};
+type BlogsProps = object;
 
 // const posts = [
 //   {
